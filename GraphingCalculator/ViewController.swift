70edd39b2f361f33acb115eed3c3e6c031ac1187
tue41582 <tue41582@tue41582s-iMac.local>
2017-03-26 04:51:08 -0400

    Initial Commit

---
+++ GraphingCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GraphingCalculator
+//
+//  Created by tue41582 on 3/26/17.
+//  Copyright © 2017 tue41582. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
